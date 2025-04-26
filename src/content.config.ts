d9a660cfc11fd686caa22f1b5032a45508cc55a8
Tsuyoshi Matsudate <t.matsudate@gmail.com>
2025-04-26 11:25:54 +0900

    Added: 現在着手中の他のプロジェクト(外部URL)

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,24 +2,24 @@ import { defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const cards = defineCollection({
-    loader: glob({
-        pattern: "**\/*.json",
-        base: "./src/content/cards",
-    }),
+  loader: glob({
+    pattern: "**\/*.json",
+    base: "./src/content/cards",
+  }),
 });
 
 const headings = defineCollection({
-    loader: glob({
-        pattern: "**\/*.json",
-        base: "./src/content/headings"
-    }),
+  loader: glob({
+    pattern: "**\/*.json",
+    base: "./src/content/headings"
+  }),
 });
 
 const repositories = defineCollection({
-    loader: glob({
-        pattern: "**\/*.json",
-        base: './src/content/repositories'
-    }),
+  loader: glob({
+    pattern: "**\/*.json",
+    base: './src/content/repositories'
+  }),
 });
 
 export const collections = { cards, headings, repositories };
